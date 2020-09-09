192297880f505c68d7f72de01161c1c8b170e114
BesedaDi <dianabesedina223@gmail.com>
2020-09-09 10:29:38 +0700

    cointinue

--- pr_dec/pr_decorator.py
+++ pr_dec/pr_decorator.py
@@ -4,15 +4,16 @@ import functools
 def rec(func): # декоратор должен расширять функционал
     @functools.wraps(func)
     def decorator(func):
+        @functools.wraps(func)
         def wrapped(*args, **kwargs):
             result = func(*args, **kwargs)
-            with open(file_name, 'w') as f:
-                json.dump(result, f)
+            with open('sw_comp.json', 'w') as f:
+                json.dump(list(result), f)
             return result
         return wrapped
     return decorator
 
-@rec
+@rec('sw_comp.json')
 def comparison(*args):
     return max(args)
 
