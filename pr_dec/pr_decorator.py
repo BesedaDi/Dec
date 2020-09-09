a50d530fe38e4b623f492286b191ae6c83b7b43a
BesedaDi <dianabesedina223@gmail.com>
2020-09-09 10:06:40 +0700

    cointinue

---
+++ pr_dec/pr_decorator.py
@@ -0,0 +1,23 @@
+import json
+import functools
+
+
+def rec(func):
+    @functools.wraps(func)
+    def decorator(func):
+        def wrapped(*args, **kwargs):
+            result = func(*args, **kwargs)
+            with open(file_name, 'w') as f:
+                file_out = json.load(f)
+                f.write(str(result))
+            return result
+        return wrapped
+    return decorator
+
+@rec('comp.json')
+def comparison(*args):
+    return max(args)
+
+
+
+print(comparison.__name__)
